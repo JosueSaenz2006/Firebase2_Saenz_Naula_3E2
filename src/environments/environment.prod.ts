f61a8d3d1d1454d576ce47493c6cd50aaf317ecf
JosueSaenz <josuesaenz885@gmail.com>
2023-05-14 22:15:40 -0500

    Producto Final

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyA0wTA0qs1mBl2wlos-FLheAs1tAdHTZRk",
+  authDomain: "insumo2saenznaulag2.firebaseapp.com",
+  projectId: "insumo2saenznaulag2",
+  storageBucket: "insumo2saenznaulag2.appspot.com",
+  messagingSenderId: "556492298046",
+  appId: "1:556492298046:web:b859ed4d84c2f8b846be6f"
+  }
+};
